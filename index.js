8032e984b6dd3fd2987bb502ac68356c8b5d0399
Mikko <mkarvonen34@gmail.com>
2021-06-13 20:57:44 +0300

    Serve todo items from endpoint
    Html rendering from string is too much of a hack. Let's have frontend in own project.

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const html_generator = require("./html_generator");
 const listItems = [];
 
 const listener = (request, response) => {
-  if (request.url === "/add_note" && request.method === "POST") {
-    let body = "";
-    request.on("data", function (data) {
-      body = data.toString();
-    });
-    request.on("end", function () {
-      listItems.push(body);
-      console.log(listItems);
-    });
-  } else if (request.url === "/" && request.method === "GET") {
-    const html = html_generator.get_html(listItems);
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(html);
+  if (request.method === "GET") {
+    if (request.url === "/notes") {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Max-Age": 2592000,
+      };
+      response.writeHead(200, headers, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(listItems));
+    } else if (request.url === "/") {
+      const html = html_generator.get_html(listItems);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(html);
+    }
+  } else if (request.method === "POST") {
+    if (request.url === "/add_note") {
+      let body = "";
+      request.on("data", function (data) {
+        body = data.toString();
+      });
+      request.on("end", function () {
+        listItems.push(body);
+        console.log(listItems);
+      });
+    }
   }
 };
 
